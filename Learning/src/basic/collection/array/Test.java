6933118cf9bda7d749fde0edf503eb6ed0b4be99
hspcadmin <hspcadmin@192.168.133.39>
2020-07-28 13:54:35 +0800

    eclipse上提交

---
+++ Learning/src/basic/collection/array/Test.java
@@ -0,0 +1,17 @@
+package basic.collection.array;
+
+public class Test {
+
+    public static void main(String[] args) throws Throwable {
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        list.add(10);
+        list.add(20);
+        list.add(30);
+
+        System.out.println(list);
+        list.remove(1);
+        System.out.println(list);
+        
+    }
+}
